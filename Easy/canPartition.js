743888c3445ee70e9f6dcd1907ac787e8fa3b5c8
Itamar Cohen <45041126+itamarc101@users.noreply.github.com>
2025-07-14 17:52:25 +0300

    solution

--- Easy/canPartition.js
+++ Easy/canPartition.js
@@ -13,8 +13,22 @@ The array may contain duplicates.
 Multiple solutions can exist, any solution is sufficient to return true.
 */
 
-function canPartition( /*args*/ ) {
-  //your code
+function canPartition(arr) {
+  let totalProduct = 1
+  let zeroCount = 0
+  if (arr.length < 2) return false
+  for (let num of arr) {
+    if (num === 0) {
+      zeroCount++
+      if (zeroCount > 1) return true
+    }
+    totalProduct *= num
+  }
+  if (zeroCount === 1 ) return false
+  for (let num of arr) {
+    if (totalProduct / num === num) return true
+  }
+  return false
 }
 
 exports.solution = canPartition;
